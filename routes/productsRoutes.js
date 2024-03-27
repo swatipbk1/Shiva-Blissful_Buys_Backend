77931e91af8885f6d4c34d41b592b3e423deb3e7
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-26 23:38:30 -0700

    c

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -63,4 +63,17 @@ router.patch('/:id', async (req, res) => {
 // DELETE a product
 router.delete('/:id', async (req, res) => {
     try {
-       
+        const productId = req.params.id;
+        const deletedProduct = await Product.findByIdAndDelete(productId);
+        if (!deletedProduct) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+        res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+module.exports = router;
