a4c68ecddcddfcd2a33565097ef92abd1d118eb1
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-26 23:36:18 -0700

    c

---
+++ models/products.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+// Define a schema for the product
+const productSchema = new mongoose.Schema({
+    prod_id: {  // Rename _id to prod_id
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    detailUrl: {
+        type: String,
+        required: true
+    },
+    title: {
+        shortTitle: {
+            type: String,
+            required: true
+        },
+        longTitle: {
+            type: String,
+            required: true
+        }
+    },
+    price: {
+        mrp: {
+            type: Number,
+            required: true
+        },
+        cost: {
+            type: Number,
+            required: true
+        },
+        discount: {
+            type: String,
+            required: true
+        }
+    },
+    description: {
+        type: String,
+     
