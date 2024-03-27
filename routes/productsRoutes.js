e01d281bbc323eced050acc662608fa2ec250062
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-26 23:30:37 -0700

    c

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -67,4 +67,13 @@ router.delete('/:id', async (req, res) => {
         const deletedProduct = await Product.findByIdAndDelete(productId);
         if (!deletedProduct) {
             return res.status(404).json({ error: 'Product not found' });
-        
+        }
+        res.status(200).json({ message: 'Product deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+module.exports = router;
